4fb4cb4a3034d4fc8d4d8616713473ec10eebb0d
MaulikRakholiya99 <82446499+MaulikRakholiya99@users.noreply.github.com>
2021-04-20 12:57:18 +0530

    final test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+var express = require('express');
 var app = require('express')();
+var path = require('path');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 const port = process.env.port || 8000;
+const publicPath = path.join(__dirname, "../public")
+
+app.use('/public', express.static(publicPath));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
+    // res.send("hello form maulik");
 });
 
 server.listen(port);
