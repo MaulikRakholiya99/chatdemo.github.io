fc09d0f34bf54e6b0d3c14e3e913bd7012e586ec
MaulikRakholiya99 <82446499+MaulikRakholiya99@users.noreply.github.com>
2021-04-27 11:25:39 +0530

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -32,6 +32,6 @@ socket.on('user-joined', name => {
 
 socket.on('receive', data => {
     append(`${data.name}: ${data.message}`, 'left');
-    var audio = new Audio('Ding-sound-effect.mp3');
+    var audio = new Audio('../Ding-sound-effect.mp3');
 audio.play();
 });
